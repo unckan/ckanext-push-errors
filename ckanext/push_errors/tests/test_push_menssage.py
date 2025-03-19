48b4af99ee0d7d5e9174027b842b25513040999b
german <germankay@gmail.com>
2025-03-19 12:30:01 -0300

    fixes

--- ckanext/push_errors/tests/test_push_menssage.py
+++ ckanext/push_errors/tests/test_push_menssage.py
@@ -1,20 +1,16 @@
+import pytest
 from unittest.mock import patch, MagicMock
 from ckanext.push_errors.logging import push_message
 
 
-@patch('ckanext.push_errors.logging.toolkit')
+@pytest.mark.ckan_config('ckanext.push_errors.url', 'https://fake-url.org')
+@pytest.mark.ckan_config('ckan.site_url', 'https://mysite.org')
+@pytest.mark.ckan_config('ckanext.push_errors.method', 'POST')
+@pytest.mark.ckan_config('ckanext.push_errors.headers', '{}')
+@pytest.mark.ckan_config('ckanext.push_errors.data', '{}')
+@pytest.mark.ckan_config('ckanext.push_errors.message_title', 'Test Message {site_url} - {now}')
 @patch('ckanext.push_errors.logging.requests.post')
-def test_push_message_success(mock_post, mock_toolkit):
-    # Configuración de mocks
-    mock_toolkit.config.get.side_effect = lambda key, default=None: {
-        'ckanext.push_errors.url': 'https://fake-url.org',
-        'ckan.site_url': 'https://mysite.org',
-        'ckanext.push_errors.method': 'POST',
-        'ckanext.push_errors.headers': '{}',
-        'ckanext.push_errors.data': '{}',
-        'ckanext.push_errors.message_title': 'Test Message {site_url} - {now}'
-    }.get(key, default)
-
+def test_push_message_success(mock_post):
     # Simular respuesta exitosa
     mock_response = MagicMock()
     mock_response.status_code = 200
@@ -30,12 +26,9 @@ def test_push_message_success(mock_post, mock_toolkit):
     assert 'Message received' in response.text
 
 
-@patch('ckanext.push_errors.logging.toolkit')
+@pytest.mark.ckan_config('ckanext.push_errors.url', None)  # No se define URL
 @patch('ckanext.push_errors.logging.requests.post')
-def test_push_message_no_url(mock_post, mock_toolkit):
-    # Configuración sin URL
-    mock_toolkit.config.get.return_value = None
-
+def test_push_message_no_url(mock_post):
     response = push_message("Mensaje sin URL")
 
     # Verificar que no se hizo ninguna solicitud
